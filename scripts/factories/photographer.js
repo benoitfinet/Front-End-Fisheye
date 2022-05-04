45443b168382c57526faff6f70f558f0bb1166b1
benoitfinet <benoitfinetpro@gmail.com>
2022-05-04 13:32:14 +0200

    home page and some personal page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,43 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+class photographerFactory {
+    constructor(photographer) {
+        this._photographer = photographer
+        this.$wrapperHome = document.createElement('div')
+        this.$wrapperPage = document.createElement('div')
     }
-    return { name, picture, getUserCardDOM }
+
+    createPhotographerHome() {
+        const photographer = `
+        <a href ='photographer.html?id=${this._photographer.id}' aria-label='Lien vers la page de ${this._photographer.name}'>
+            <article>
+                <img src='../assets/photographers/${this._photographer.portrait}' alt='Photo de ${this._photographer.name}'>
+                <h2>${this._photographer.name}</h2>
+                <h3>${this._photographer.city + ', ' + this._photographer.country}</h3>
+                <p>${this._photographer.tagline}</p>
+                <span>${this._photographer.price}€/jour</span>
+            </article>
+        </a>
+        `
+        this.$wrapperHome.innerHTML = photographer
+
+        return this.$wrapperHome
+    }
+
+    createPhotographerPage() {
+        const photographer = `
+        <a href ='photographer.html?id=${this._photographer.id}' aria-label='Lien vers la page de ${this._photographer.name}'>
+            <article>
+                <img src='../assets/photographers/${this._photographer.portrait}' alt='Photo de ${this._photographer.name}'>
+                <h2>${this._photographer.name}</h2>
+                <h3>${this._photographer.city + ', ' + this._photographer.country}</h3>
+                <p>${this._photographer.tagline}</p>
+                <span>${this._photographer.price}€/jour</span>
+            </article>
+        </a>
+        `
+        this.$wrapperPage.innerHTML = photographer
+
+        return this.$wrapperPage
+    }
 }
+
+export {photographerFactory};
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,63 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+import { photographerFactory } from "../factories/photographer.js";
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+export async function getPhotographers() {
+    let data = await fetch('/data/photographers.json')
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        return data
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
+    let dataPhotographers = data.photographers;
     
-    init();
+    let photographers = [];
     
+    dataPhotographers.forEach((item) => {
+        const photographe = new photographerFactory(item, "json")
+        photographers.push(photographe)
+    })
+
+    return photographers
+}
+    
+async function displayPhotographersHomeData(item) {
+    let nodes = document.querySelector('.photographer_section');
+    
+    item.forEach((photographer) => {
+        const userCardDOM = photographer.createPhotographerHome();
+        nodes.append(userCardDOM);
+    });
+};
+
+async function displayPhotographersPageData() {
+    const photographersD = await getPhotographers();
+    let nodes = document.querySelector('.photograph-header');
+    
+    var url_string = window.location.href
+    var objectURL = new URL(url_string);
+    var pageID = objectURL.searchParams.get("id");
+    
+    let photographerByID = null
+    
+    photographersD.forEach((photographer) => {
+        if(photographersD.id === pageID) {
+            photographerByID = photographersD
+            const userCardDOM = photographer.createPhotographerPage();
+            nodes.append(userCardDOM);
+        }
+    });
+};
+
+async function init() {
+    const photographers = await getPhotographers();
+    const photographerByID = await displayPhotographersPageData();
+    
+    displayPhotographersHomeData(photographers);
+    displayPhotographersPageData(photographerByID);
+};
+
+init();
